e1ba78f41d6579502d2ffa576997839a6bf79163
Geek <hazeebville@gmail.com>
2024-11-13 23:23:19 +0000

    Appearance

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ arrows.forEach((right, index) => {
     MovieLists[index].style.transform = `translateX(-100px)`;
   });
 });
+
+const ball = document.querySelector(".toggleball");
+const items = document.querySelectorAll(
+  ".container, .movie-list-title, .navbar, .navbar a, .profcss, .proftext,  .sidebar, .material-symbols-rounded, .Toggle, .toggleball"
+);
+
+ball.addEventListener("click", () => {
+  Array.from(items).forEach((item) => {
+    item.classList.toggle("active");
+  });
+  ball.classList.toggle("active");
+});
